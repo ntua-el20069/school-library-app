12d395c17fe7e6e5128a505ecf4d9af0f128fdd4
nikolaospapa3 <nikolaospapa3@gmail.com>
2023-04-08 20:00:34 +0300

    made a simple sign in and added some routes

--- venv/src/App.py
+++ venv/src/App.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+# ...
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return '<h1>Hello</h1>'
+    return render_template('home.html')
 
 @app.route("/sample")
 def sample():
@@ -14,13 +16,39 @@ def sample():
 # This is a simple routing example 
 # If you want to make an html page, you should put it into directory 'templates'
 
-@app.route("/signup")
-def signup():
-    return render_template('sign-up.html')
+#@app.route("/signup")
+#def signup():
+#    return render_template('sign-up.html')
 
-@app.route("/signin")
-def signin():
-    return render_template('sign-in.html')
+@app.route("/signup", methods=['GET', 'POST'])
+def signup_form_redirect():
+    if request.method == 'POST':
+        return redirect(url_for('notApprovedUser'))
+    else:
+        return render_template('sign-up.html')
+    
+#@app.route("/signin")
+#def signin():
+#        return render_template('sign-in.html')
+    
+@app.route("/signin", methods=['GET', 'POST'])
+def handle_signin():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if username == 'nikospapa':
+            return redirect(url_for('student'))
+        else:
+            return redirect(url_for('notApprovedUser'))
+    else:
+        return render_template('sign-in.html')
+
+@app.route("/not-approved-user")
+def notApprovedUser():
+    return render_template('not-approved-user.html')
+
+@app.route("/student")
+def student():
+    return render_template('student.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
